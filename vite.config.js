a3b692bdb948350dc27467e251d0602862d07656
Max Coelho dos Santos <maxstos@gmail.com>
2025-04-07 12:03:42 +0200

    change path rectangle abuela

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html', // Entrada principal
+        location: './location.html', // Entrada adicional
+      },
+    },
+  },
+
 });
